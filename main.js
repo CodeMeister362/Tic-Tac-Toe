92a47830eea94a20ddcabbd982da465f5e3e426f
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-03-05 07:20:05 -0800

    Adds progress on updating game boad.

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ var player1 = new Player ('Player1', 'ONE');
 var player2 = new Player ('Player2', 'TWO');  
 var game = new Game (player1, player2); 
 
+// QuerySelectors
 var boardGame = document.querySelector('.all-box'),
     currentPlayer = document.querySelector('.js-whose-turn');
 
+// Event Listener
+    boardGame.forEach(function(item) {
+      item.addEventListener('click', function(event) {
+        updatePlayer()
+        updateToken()
+      })
+    })
 
-boardGame.addEventListener('click', updatePlayer);
-
-
+// Event handlers
 function updatePlayer() {
- console.log(game.currentTurn())
- currentPlayer.innerHTML = `<div class="whose-turn js-whose-turn">It's ${game.turn.id} turn!</div>`;
+  console.log(game.currentTurn())
+
+  currentPlayer.innerHTML = `
+  <div class="whose-turn js-whose-turn">It's ${game.turn.id} turn!</div>
+  `;
+}
+
+function updateToken() {
+  console.log(game.turn.token)
+  boxToken.innerHTML = `<div class="box border-top js-2">${game.turn.token}</div>`
 }
  
