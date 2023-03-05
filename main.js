e07b851e0273c973580a1d2212069b53c5871603
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-03-05 07:45:08 -0800

    Adds updated tokens on console.log just need to change DOM

--- main.js
+++ main.js
@@ -3,27 +3,28 @@ var player2 = new Player ('Player2', 'TWO');
 var game = new Game (player1, player2); 
 
 // QuerySelectors
-var boardGame = document.querySelector('.all-box'),
+var boardGame = document.querySelectorAll('.all-box'),
     currentPlayer = document.querySelector('.js-whose-turn');
 
 // Event Listener
     boardGame.forEach(function(item) {
       item.addEventListener('click', function(event) {
-        updatePlayer()
-        updateToken()
+        var source = event.target || event.srcElement;
+        console.log(source)
+        updatePlayer(event)
+        updateToken(event)
       })
     })
 
 // Event handlers
 function updatePlayer() {
   console.log(game.currentTurn())
-
   currentPlayer.innerHTML = `
   <div class="whose-turn js-whose-turn">It's ${game.turn.id} turn!</div>
   `;
 }
 
-function updateToken() {
+function updateToken(boxToken) {
   console.log(game.turn.token)
   boxToken.innerHTML = `<div class="box border-top js-2">${game.turn.token}</div>`
 }
